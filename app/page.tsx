fcd688250b66729295a5edb21a1e198a23421fb7
Alef185 <alefsouzamac1023@gmail.com>
2024-02-22 13:11:27 -0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -21,43 +21,43 @@ import {
 } from "@nextui-org/react";
 import Image from "next/image";
 
-const HeaderNavBar = () => {
-  return (
-    <Navbar shouldHideOnScroll>
-      <NavbarBrand>
-        <Image src="./vercel.svg" alt="logo" width={40} height={40} />
-        <p className="font-bold text-inherit">ACME</p>
-      </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Features
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#">
-            Integrations
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Button as={Link} color="primary" href="#" variant="flat">
-            Sign Up
-          </Button>
-        </NavbarItem>
-      </NavbarContent>
-    </Navbar>
-  );
-};
+// const HeaderNavBar = () => {
+//   return (
+//     <Navbar shouldHideOnScroll>
+//       <NavbarBrand>
+//         <Image src="./vercel.svg" alt="logo" width={40} height={40} />
+//         <p className="font-bold text-inherit">ACME</p>
+//       </NavbarBrand>
+//       <NavbarContent className="hidden sm:flex gap-4" justify="center">
+//         <NavbarItem>
+//           <Link color="foreground" href="#">
+//             Features
+//           </Link>
+//         </NavbarItem>
+//         <NavbarItem isActive>
+//           <Link href="#" aria-current="page">
+//             Customers
+//           </Link>
+//         </NavbarItem>
+//         <NavbarItem>
+//           <Link color="foreground" href="#">
+//             Integrations
+//           </Link>
+//         </NavbarItem>
+//       </NavbarContent>
+//       <NavbarContent justify="end">
+//         <NavbarItem className="hidden lg:flex">
+//           <Link href="#">Login</Link>
+//         </NavbarItem>
+//         <NavbarItem>
+//           <Button as={Link} color="primary" href="#" variant="flat">
+//             Sign Up
+//           </Button>
+//         </NavbarItem>
+//       </NavbarContent>
+//     </Navbar>
+//   );
+// };
 
 export default function Home() {
   return (
